2ed0d69eb1712ba5460723d59a0de5f5ca5fe299
Antoinehrt <hauret.antoine@gmail.com>
2025-11-28 11:03:51 +0100

    feat(Header): add PageTitleService to manage and observe page titles

---
+++ src/app/core/services/page-title.service.ts
@@ -0,0 +1,15 @@
+﻿import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class PageTitleService {
+  private pageTitleSubject = new BehaviorSubject<string>('Home');
+  public pageTitle$ = this.pageTitleSubject.asObservable();
+
+  setPageTitle(title: string) {
+    this.pageTitleSubject.next(title);
+  }
+}
+
