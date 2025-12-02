22b0574fc91b00b95c287e58ab5d67d4e41ca09f
antoinehrt <hauret.antoine@gmail.com>
2025-12-02 19:00:59 +0100

    feat(WalletCreation): add wallet creation component with template and styles

---
+++ src/app/pages/wallet-creation/wallet-creation.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-wallet-creation',
+  imports: [],
+  templateUrl: './wallet-creation.html',
+  styleUrl: './wallet-creation.css',
+})
+export class WalletCreation {
+
+}
