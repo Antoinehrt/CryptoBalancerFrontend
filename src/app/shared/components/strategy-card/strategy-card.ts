38a5c2446f225c2c3f569b707054982a5d10c86f
antoinehrt <hauret.antoine@gmail.com>
2025-12-05 12:01:33 +0100

    feat(StrategyCard): add initial implementation of strategy card component with template and styles

---
+++ src/app/shared/components/strategy-card/strategy-card.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-strategy-card',
+  imports: [],
+  templateUrl: './strategy-card.html',
+  styleUrl: './strategy-card.css',
+})
+export class StrategyCard {
+
+}
