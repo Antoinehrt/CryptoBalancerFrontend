8c68f34fc9b08b40d954e046511ede80976ed834
antoinehrt <hauret.antoine@gmail.com>
2025-12-05 18:19:32 +0100

    feat(StrategyCard)?: Check point
    need to change computer

--- src/app/shared/components/strategy-card/strategy-card.ts
+++ src/app/shared/components/strategy-card/strategy-card.ts
@@ -1,11 +1,45 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
+import {NgIf} from '@angular/common';
+import {RiskProfile} from '../risk-profile/risk-profile';
+import {MatButton} from '@angular/material/button';
 
 @Component({
   selector: 'app-strategy-card',
-  imports: [],
+    imports: [
+        NgIf,
+        RiskProfile,
+        MatButton
+    ],
   templateUrl: './strategy-card.html',
   styleUrl: './strategy-card.css',
 })
 export class StrategyCard {
 
+    @Input()
+    icon?: string;
+    @Input()
+    title!: string;
+    @Input()
+    description?: string;
+    @Input()
+    marketType?: string;
+    @Input()
+    twoYear?: number;
+    @Input()
+    pnl?: number;
+    @Input()
+    riskProfile!: number;
+
+    @Input()
+    showModifyButton = false;
+    @Input()
+    onModify?: () => void;
+
+    @Input()
+    showChooseButton = false;
+    @Input()
+    onChoose?: () => void;
+
+
+    protected readonly RiskProfile = RiskProfile;
 }
