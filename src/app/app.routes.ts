0b111107c188c1ddabcf7ab9525804b5f348102d
AntoineHrt <hauret.antoine@gmail.com>
2025-12-08 17:44:51 +0100

    feat(Authentication): add login page to the routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import {Home} from './pages/home/home';
 import {WalletCreation} from './pages/wallet-creation/wallet-creation';
 import {Test} from './pages/test/test';
+import {Login} from './pages/login/login';
 
 export const routes: Routes = [
     {path: '', component: Home},
     {path: 'home', component: Home},
+    {path: 'login', component: Login},
     {path: 'wallet-creation', component: WalletCreation},
     {path: 'test', component: Test},
     //{path: '404-not-found', component: NotFoundComponent},
