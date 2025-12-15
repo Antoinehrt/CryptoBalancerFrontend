ea6eead667ab857128cc2ff65ab1b87e91e8a59f
antoinehrt <hauret.antoine@gmail.com>
2025-12-15 23:28:53 +0100

    feat(Authentication): update navbar to use AuthService for logout functionality

--- src/app/shared/components/navbar/navbar.ts
+++ src/app/shared/components/navbar/navbar.ts
@@ -3,6 +3,7 @@ import {MatSidenav, MatSidenavContainer, MatSidenavContent} from '@angular/mater
 import {BreakpointObserver, Breakpoints} from '@angular/cdk/layout';
 import {RouterLink} from '@angular/router';
 import {MatListItem} from '@angular/material/list';
+import {AuthService} from '../../../core/services/auth/auth.service';
 
 @Component({
     selector: 'app-navbar',
@@ -20,7 +21,7 @@ export class Navbar {
     @ViewChild('sidenav') sidenav!: MatSidenav;
     isHandset = false;
 
-    constructor(private breakpointObserver: BreakpointObserver) {
+    constructor(private breakpointObserver: BreakpointObserver, private _authenticationService: AuthService) {
         this.breakpointObserver.observe([Breakpoints.Handset]).subscribe(result => {
             this.isHandset = result.matches;
             if (this.sidenav) {
@@ -31,12 +32,15 @@ export class Navbar {
     }
 
     ngAfterViewInit() {
-        // ensure initial state after ViewChild is ready
         if (this.isHandset) this.sidenav.close();
         else this.sidenav.open();
     }
 
     closeIfHandset() {
         if (this.isHandset) this.sidenav.close();
+    }
+
+    logout() {
+        this._authenticationService.logout();
     }
 }
