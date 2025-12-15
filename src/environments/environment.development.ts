eb95dcdd5ef30f4bd6b27e6e692ffcc40d0e1e69
antoinehrt <hauret.antoine@gmail.com>
2025-12-15 19:21:16 +0100

    feat(Authentication): add Google OAuth endpoints to environment configurations

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,6 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://127.0.0.1:8000/api/v1/',
+    apiUrl: 'http://localhost:8000/api/v1/',
+    GOOGLE_AUTHORIZE:"http://localhost:8000/api/v1/auth/google/login",
+    GOOGLE_CALLBACK:"http://localhost:8000/api/v1/auth/google/callback",
 };
