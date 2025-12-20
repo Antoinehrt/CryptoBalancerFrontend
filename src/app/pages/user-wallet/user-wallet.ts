74adf90fefacd947212a560db54c687d77565a66
AntoineHrt <hauret.antoine@gmail.com>
2025-12-20 18:27:43 +0100

    feat(UserWallet): create user wallet component with template and styles

---
+++ src/app/pages/user-wallet/user-wallet.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-wallet',
+  imports: [],
+  templateUrl: './user-wallet.html',
+  styleUrl: './user-wallet.css',
+})
+export class UserWallet {
+
+}
