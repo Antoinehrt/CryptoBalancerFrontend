c3b785dc9151fbed67a772444073617c3c3c9c18
AntoineHrt <hauret.antoine@gmail.com>
2025-12-23 23:58:46 +0100

    feat(UserWallet): update wallet route guards to use hasWalletGuard and noWalletGuard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,16 +5,17 @@ import {Test} from './pages/test/test';
 import {Login} from './pages/login/login';
 import {Profile} from './pages/profile/profile';
 import {authGuard} from './core/guard/auth/auth-guard';
-import {walletExistsGuard} from './core/guard/wallet-exists/wallet-exists-guard';
 import {UserWallet} from './pages/user-wallet/user-wallet';
+import {hasWalletGuard} from './core/guard/has-wallet/has-wallet-guard';
+import {noWalletGuard} from './core/guard/no-wallet/no-wallet-guard';
 
 export const routes: Routes = [
     {path: '', component: Home},
     {path: 'home', component: Home},
     {path: 'login', component: Login},
     {path: 'profile', component: Profile, canActivate: [authGuard]},
-    {path: 'wallet-creation', component: WalletCreation, canActivate: [authGuard, walletExistsGuard]},
-    {path: 'wallet', component: UserWallet, canActivate: [authGuard]},
+    {path: 'wallet-creation', component: WalletCreation, canActivate: [authGuard, noWalletGuard]},
+    {path: 'wallet', component: UserWallet, canActivate: [authGuard, hasWalletGuard]},
     {path: 'test', component: Test},
     //{path: '404-not-found', component: NotFoundComponent},
     {path: '**', redirectTo: '404-not-found'}
