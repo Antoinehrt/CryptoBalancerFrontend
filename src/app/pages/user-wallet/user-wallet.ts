2a66e669b64450374dfca6bd7bebe892e6332ef2
AntoineHrt <hauret.antoine@gmail.com>
2025-12-23 23:54:59 +0100

    feat(UserWallet): integrate wallet and user services with page title setup

--- src/app/pages/user-wallet/user-wallet.ts
+++ src/app/pages/user-wallet/user-wallet.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
+import {PageTitleService} from '../../core/services/page-title/page-title.service';
+import {WalletService} from '../../core/services/wallet/wallet.service';
+import {UserService} from '../../core/services/user/user.service';
+import {UserDto} from '../../core/dto/user-dto';
+import {WalletDto} from '../../core/dto/wallet-dto';
+import {switchMap} from 'rxjs';
 
 @Component({
   selector: 'app-user-wallet',
@@ -7,5 +13,23 @@ import { Component } from '@angular/core';
   styleUrl: './user-wallet.css',
 })
 export class UserWallet {
+    private _pageTitleService = inject(PageTitleService);
+    private _userService = inject(UserService);
+    private _walletService = inject(WalletService);
 
+    wallet?: WalletDto;
+    user?: UserDto;
+
+    constructor() {
+        this._pageTitleService.setPageTitle('My Wallet');
+        this._userService.getCurrentUser().pipe(
+            switchMap(user => {
+                this.user = user;
+                return this._walletService.getWalletFromUser(user.id);
+            })
+        ).subscribe(wallet => {
+            this.wallet = wallet;
+        });
+        console.log(this.wallet);
+    }
 }
