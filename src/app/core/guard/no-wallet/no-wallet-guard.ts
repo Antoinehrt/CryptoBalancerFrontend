18feafd2a42c163fb3642c15bb62acbce9fff317
AntoineHrt <hauret.antoine@gmail.com>
2025-12-23 23:58:12 +0100

    feat(UserWallet): rename wallet guard files and implement hasWalletGuard and noWalletGuard functionality

---
+++ src/app/core/guard/no-wallet/no-wallet-guard.ts
@@ -0,0 +1,26 @@
+import {CanActivateFn, Router} from '@angular/router';
+import {inject} from '@angular/core';
+import {AuthService} from '../../services/auth/auth.service';
+import {WalletService} from '../../services/wallet/wallet.service';
+import {catchError, map, switchMap} from 'rxjs/operators';
+import {of} from 'rxjs';
+
+export const noWalletGuard: CanActivateFn = (route, state) => {
+    const authService = inject(AuthService);
+    const walletService = inject(WalletService);
+    const router = inject(Router);
+
+    return authService.getCurrentUser().pipe(
+        switchMap(user => walletService.walletExists(user.id).pipe(
+            map(exists => {
+                if (exists) return router.createUrlTree(['/wallet']);
+                return true;
+            }),
+            catchError(err => {
+                if (err.status === 401) return of(router.createUrlTree(['/login']));
+                return of(true);
+            })
+        )),
+        catchError(() => of(router.createUrlTree(['/login'])))
+    );
+};
