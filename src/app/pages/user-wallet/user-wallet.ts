bd408f6c22b34e4559498145573d3a77dc00d4e7
AntoineHrt <hauret.antoine@gmail.com>
2026-01-08 16:25:50 +0100

    feat(UserWallet): implement asset loading and display in user wallet component

--- src/app/pages/user-wallet/user-wallet.ts
+++ src/app/pages/user-wallet/user-wallet.ts
@@ -1,35 +1,39 @@
-import {Component, inject} from '@angular/core';
+import {Component, OnInit, signal} from '@angular/core';
+import {CommonModule, CurrencyPipe, DecimalPipe} from '@angular/common';
 import {PageTitleService} from '../../core/services/page-title/page-title.service';
-import {WalletService} from '../../core/services/wallet/wallet.service';
-import {UserService} from '../../core/services/user/user.service';
-import {UserDto} from '../../core/dto/user-dto';
-import {WalletDto} from '../../core/dto/wallet-dto';
-import {switchMap} from 'rxjs';
+import {WalletFacadeService} from '../../core/services/wallet/wallet-facade.service';
+import {Asset} from '../../core/models/asset';
 
 @Component({
-  selector: 'app-user-wallet',
-  imports: [],
-  templateUrl: './user-wallet.html',
-  styleUrl: './user-wallet.css',
+    selector: 'app-user-wallet',
+    standalone: true,
+    imports: [
+        CommonModule,
+        CurrencyPipe,
+        DecimalPipe
+    ],
+    templateUrl: './user-wallet.html',
+    styleUrl: './user-wallet.css',
 })
-export class UserWallet {
-    private _pageTitleService = inject(PageTitleService);
-    private _userService = inject(UserService);
-    private _walletService = inject(WalletService);
+export class UserWallet implements OnInit {
+    assets = signal<Asset[]>([]);
+    isLoading = signal(true);
 
-    wallet?: WalletDto;
-    user?: UserDto;
+    constructor(
+        private _walletBusinessService: WalletFacadeService,
+        private _pageTitle: PageTitleService
+    ) {
+    }
 
-    constructor() {
-        this._pageTitleService.setPageTitle('My Wallet');
-        this._userService.getCurrentUser().pipe(
-            switchMap(user => {
-                this.user = user;
-                return this._walletService.getWalletFromUser(user.id);
-            })
-        ).subscribe(wallet => {
-            this.wallet = wallet;
+    ngOnInit(): void {
+        this._pageTitle.setPageTitle('My Wallet');
+
+        this._walletBusinessService.loadWallet().subscribe({
+            next: assets => {
+                this.assets.set(assets);
+                this.isLoading.set(false);
+            },
+            error: () => this.isLoading.set(false)
         });
-        console.log(this.wallet);
     }
 }
