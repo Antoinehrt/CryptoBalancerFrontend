366e06acba73cb11bfa9a03f29ace781fe7a50a9
AntoineHrt <hauret.antoine@gmail.com>
2026-01-22 15:59:13 +0100

    feat(Backtest): implement loading state and display strategy cards with KPIs

--- src/app/pages/backtest/backtest.ts
+++ src/app/pages/backtest/backtest.ts
@@ -1,10 +1,15 @@
-import {Component, inject, OnInit} from '@angular/core';
+import {Component, inject, OnInit, signal} from '@angular/core';
 import {UserService} from '../../core/services/user/user.service';
 import {BacktestCalculationService} from '../../core/services/backtest/backtest-calculation.service';
+import {StrategyCard} from '../../shared/components/strategy-card/strategy-card';
+import {KpisModel} from '../../core/models/kpis.model';
+import {PageTitleService} from '../../core/services/page-title/page-title.service';
 
 @Component({
   selector: 'app-backtest',
-  imports: [],
+    imports: [
+        StrategyCard
+    ],
   templateUrl: './backtest.html',
   styleUrl: './backtest.css',
 })
@@ -12,10 +17,21 @@ export class Backtest implements OnInit {
 
     private _backtestCalculationService = inject(BacktestCalculationService);
     private _userService = inject(UserService);
+    private _pageTitleService = inject(PageTitleService)
+
+    protected isLoading = signal(true);
+    protected kpis: KpisModel[] = [];
+
+    //TODO : id user dynamic
 
     ngOnInit(): void {
-        this._backtestCalculationService.calculateKPIs(1).subscribe(response => {
-            console.log(response);
+        this._pageTitleService.setPageTitle("Backtest");
+        this._backtestCalculationService.calculateKPIs(1).subscribe({
+            next: response => {
+                this.kpis = response;
+                console.log(this.kpis.at(1)?.totalCost);
+                this.isLoading.set(false);
+            }
         });
     }
 
