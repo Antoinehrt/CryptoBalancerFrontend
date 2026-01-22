734f9b93b0db6e2d1d988c6fb8406fc20d9f0132
AntoineHrt <hauret.antoine@gmail.com>
2026-01-22 15:55:56 +0100

    feat(Routes): remove unused test route from application routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,6 @@
 import {Routes} from '@angular/router';
 import {Home} from './pages/home/home';
 import {WalletCreation} from './pages/wallet-creation/wallet-creation';
-import {Test} from './pages/test/test';
 import {Login} from './pages/login/login';
 import {Profile} from './pages/profile/profile';
 import {authGuard} from './core/guards/auth/auth.guard';
@@ -18,7 +17,6 @@ export const routes: Routes = [
     {path: 'wallet-creation', component: WalletCreation, canActivate: [authGuard, noWalletGuard]},
     {path: 'wallet', component: UserWallet, canActivate: [authGuard, hasWalletGuard]},
     {path: 'backtest', component: Backtest, canActivate: [authGuard, hasWalletGuard]},
-    {path: 'test', component: Test},
     //{path: '404-not-found', component: NotFoundComponent},
     {path: '**', redirectTo: '404-not-found'}
 ];
