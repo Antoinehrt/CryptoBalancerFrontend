b34c9194839804d03be51e9788e03c7d2d1dd243
AntoineHrt <hauret.antoine@gmail.com>
2026-01-22 16:23:31 +0100

    feat(StrategyPage): enhance strategy display with dynamic data binding and styling

--- src/app/pages/strategy/strategy.ts
+++ src/app/pages/strategy/strategy.ts
@@ -1,6 +1,7 @@
-import {Component} from '@angular/core';
-import {AvailableStrategiesEnum} from '../../core/enums/available-strategies.enum';
+import {Component, inject, OnInit} from '@angular/core';
 import {StrategyCard} from '../../shared/components/strategy-card/strategy-card';
+import {StrategyModel} from '../../core/models/strategy.model';
+import {PageTitleService} from '../../core/services/page-title/page-title.service';
 
 @Component({
   selector: 'app-strategy',
@@ -11,13 +12,30 @@ import {StrategyCard} from '../../shared/components/strategy-card/strategy-card'
   styleUrl: './strategy.css',
 })
 
-export class Strategy{
+export class Strategy implements OnInit{
+    private _pageTitleService = inject(PageTitleService);
 
-    strategies: AvailableStrategiesEnum[];
+    strategies: StrategyModel[] =  [
+        {
+            id: 1,
+            icon: '',
+            title: 'Constant Mix',
+            description: 'Stratégie de rééquilibrage automatique maintenant une allocation constante entre actifs risqués et non risqués',
+            marketType: 'Volatile',
+            riskProfile: 5
+        },
+        {
+            id: 2,
+            icon: '',
+            title: 'Hold',
+            description: 'Stratégie passive d\'achat et de conservation à long terme sans rééquilibrage',
+            marketType: 'Haussier',
+            riskProfile: 3
+        }
+    ];
 
-
-    constructor() {
-        this.strategies = Object.values(AvailableStrategiesEnum);
+    ngOnInit(): void {
+        this._pageTitleService.setPageTitle("Strategies");
     }
 
 }
